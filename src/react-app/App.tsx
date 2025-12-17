54df49c28b56554e234b7a161b71e3bd41e22419
Lee <sky25777@gmail.com>
2025-12-17 18:07:49 +0900

    추천 검색어 기능 구현 및 UI/UX 개선 (키보드 이동, 레이아웃 수정)

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -88,31 +88,32 @@ function App() {
 					</a>
 				</h1>
 				<form onSubmit={(e) => handleSearch(e)} className="search-form">
-					<div className={`search-input-wrapper ${showSuggestions && suggestions.length > 0 ? 'has-suggestions' : ''}`}>
-						<svg
-							className="search-icon"
-							focusable="false"
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-						>
-							<path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-						</svg>
-						<input
-							type="text"
-							className="search-input"
-							value={query}
-							onChange={(e) => {
-                                setQuery(e.target.value);
-                                setShowSuggestions(true);
-                            }}
-                            onFocus={() => setShowSuggestions(true)}
-							onKeyDown={handleKeyDown}
-							placeholder="Search"
-							autoFocus
-						/>
-					</div>
-                    {showSuggestions && suggestions.length > 0 && (
-                        <ul className="suggestions-list">
+					<div className="search-box-container">
+						<div className={`search-input-wrapper ${showSuggestions && suggestions.length > 0 ? 'has-suggestions' : ''}`}>
+							<svg
+								className="search-icon"
+								focusable="false"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+							>
+								<path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+							</svg>
+							<input
+								type="text"
+								className="search-input"
+								value={query}
+								onChange={(e) => {
+									setQuery(e.target.value);
+									setShowSuggestions(true);
+								}}
+								onFocus={() => setShowSuggestions(true)}
+								onKeyDown={handleKeyDown}
+								placeholder="Search"
+								autoFocus
+							/>
+						</div>
+						{showSuggestions && suggestions.length > 0 && (
+							<ul className="suggestions-list">
 								{suggestions.map((suggestion, index) => (
 									<li
 										key={index}
@@ -120,19 +121,20 @@ function App() {
 										onClick={() => handleSuggestionClick(suggestion)}
 										onMouseEnter={() => setSelectedIndex(index)}
 									>
-                                    <svg 
-                                        className="suggestion-icon" 
-                                        xmlns="http://www.w3.org/2000/svg" 
-                                        viewBox="0 0 24 24"
-                                        fill="currentColor"
-                                    >
-                                        <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-                                    </svg>
-                                    {suggestion}
-                                </li>
-                            ))}
-                        </ul>
-                    )}
+										<svg 
+											className="suggestion-icon" 
+											xmlns="http://www.w3.org/2000/svg" 
+											viewBox="0 0 24 24"
+											fill="currentColor"
+										>
+											<path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+										</svg>
+										{suggestion}
+									</li>
+								))}
+							</ul>
+						)}
+					</div>
 					<div className="button-group">
 						<button type="submit" className="search-button">
 							YouTube Search
